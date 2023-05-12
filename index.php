15e1df457b390e4e7ee9c8cc4cad8af8e2da1c68
heroku20 <130670798+heroku20@users.noreply.github.com>
2023-05-12 12:02:43 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,93 @@
+ <?php
+echo '
+<!DOCTYPE html>
+<html  >
+<head>
+  <!-- Site made with Mobirise Website Builder v5.8.9, https://mobirise.com -->
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="generator" content="Mobirise v5.8.9, mobirise.com">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+  <link rel="shortcut icon" href="assets/images/logo5.png" type="image/x-icon">
+  <meta name="description" content="">
+  
+  
+  <title>Home</title>
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="assets/socicon/css/styles.css">
+  <link rel="stylesheet" href="assets/theme/css/style.css">
+  <link rel="preload" href="https://fonts.googleapis.com/css?family=Jost:100,200,300,400,500,600,700,800,900,100i,200i,300i,400i,500i,600i,700i,800i,900i&display=swap" as="style" onload="this.onload=null;this.rel=\'stylesheet\'">
+  <noscript><link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Jost:100,200,300,400,500,600,700,800,900,100i,200i,300i,400i,500i,600i,700i,800i,900i&display=swap"></noscript>
+  <link rel="preload" as="style" href="assets/mobirise/css/mbr-additional.css"><link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+
+  
+  
+  
+</head>
+<body>
+  
+  <section data-bs-version="5.1" class="content1 cid-s48udlf8KU" id="content1-8">
+    
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="title col-12 col-md-9">
+                <h3 class="mbr-section-title mbr-fonts-style align-center mb-4 display-2"><strong><u>Ikaria Juice™ Official</u></strong></h3>
+                <h4 class="mbr-section-subtitle align-center mbr-fonts-style mb-4 display-7"><div></div></h4>
+                
+            </div>
+        </div>
+    </div>
+</section>
+
+<section data-bs-version="5.1" class="header9 cid-tCZOr63FIW" id="header9-k">
+
+    
+
+    
+    
+
+    <div class="text-center container">
+        <div class="row justify-content-center">
+            <div class="col-md-12 col-lg-9">
+                <div class="image-wrap mb-4">
+                    <a href="https://cutt.ly/officialikariajuice"><img src="assets/images/ikk-1043x810.jpg" alt="Ikaria™ Lean Bellu Juice"></a>
+                </div>
+                <h1 class="mbr-section-title mbr-fonts-style mb-3 display-2"><strong>Visit Official Website</strong></h1>
+                <p class="mbr-text mbr-fonts-style display-4"><u><strong>Click Here</strong></u></p>
+                <div class="mbr-section-btn mt-3"><a class="btn btn-primary display-1" href="https://cutt.ly/officialikariajuice">Buy Now</a></div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section data-bs-version="5.1" class="footer3 cid-s48P1Icc8J" once="footers" id="footer3-i">
+
+    
+
+    
+
+    <div class="container">
+        <div class="media-container-row align-center mbr-white">
+            <div class="row row-links">
+                <ul class="foot-menu">
+                    
+                    
+                    
+                    
+                    
+                <li class="foot-menu-item mbr-fonts-style display-7"><a href="" class="text-warning">Terms &amp; Conditions</a></li><li class="foot-menu-item mbr-fonts-style display-7"><a href="" class="text-warning text-primary">Privacy</a></li><li class="foot-menu-item mbr-fonts-style display-7"><a href="" class="text-warning">Disclaimer&nbsp;</a></li></ul>
+            </div>
+            
+            <div class="row row-copirayt">
+                <p class="mbr-text mb-0 mbr-fonts-style mbr-white align-center display-7">COPYRIGHT © Ikaria Juice - ALL RIGHTS RESERVED.</p>
+            </div>
+        </div>
+    </div>
+  
+  
+</body>
+</html>
+';
+?>
